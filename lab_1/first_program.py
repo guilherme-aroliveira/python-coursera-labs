8bb14d58fd054f754246dac4abed1f548478b7c7
Guilherme Oliveira <guilherme.oliveira@waycarbon.com>
2024-05-02 11:20:06 -0300

    add lab_1

---
+++ lab_1/first_program.py
@@ -0,0 +1,36 @@
+# Exercise: Your First Program
+# print out the phrase: Hello, world! and comment it with
+# the phrase Print the traditional hello world all in one line of code
+
+print("Hello, world!")  # Print the traditional hello world
+
+
+# Exercise: Types
+# What is the data type of the result of: 6 / 2?
+
+type(6 / 2)
+
+# What is the type of the result of: 6 // 2? (Note the double slash //.)
+
+type(6 // 2)
+
+
+# Exercise: Expression
+# Let's write an expression that calculates
+# how many hours there are in 160 minutes:
+
+print(160/60)
+
+# Exercise: Expression and Variables in Python
+# What is the value of x ? where, x = 3 + 2 * 2
+
+x = 3 + 2 * 2
+print(x)
+
+# What is the value of y where y = (3 + 2) * 2?
+y = (3 + 2) * 2
+print(y)
+
+# What is the value of z where z = x + y?
+z = x + y
+print(y)
